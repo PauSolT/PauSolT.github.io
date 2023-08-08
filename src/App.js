fa95fb256fcb227db1d7375cf953876f416b1306
PauSolT <pausoltor@gmail.com>
2023-08-08 12:43:43 +0200

    Remove my-app folder

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React Hello Alexxxxxxx
+    //     </a>
+    //   </header>
+    // </div>
+    <div className="App">
+      <header>
+        <div id="headerName">
+        PAU SOLE TORRALBA
+        </div>
+        <div id="headerOptions">
+          <div>
+            HOME
+          </div>
+          <div>
+            GAMES
+          </div>
+          <div>
+            ABOUT
+          </div>
+          <div>
+            CONTACT ME
+          </div>
+        </div>
+      </header>
+      <body>
+        body
+      </body>
+      <footer>
+        footer  
+      </footer>
+    </div>
+  );
+}
+
+export default App;
