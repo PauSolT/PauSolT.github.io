a5c65cb9dcc8f28d94fcd7795da2a06cbfb7b331
PauSolT <pausoltor@gmail.com>
2023-08-09 15:06:01 +0200

    Add header

---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import './Header.css';
+
+function Header({title, genre, description, urlImage}){
+    return (
+    <header>
+        <div className="headerName">
+        PAU SOLÉ TORRALBA
+        </div>
+        <div className="headerOptions">
+          <div>
+            HOME
+          </div>
+          <div>
+            GAMES
+          </div>
+          <div>
+            ABOUT
+          </div>
+          <div>
+            CONTACT ME
+          </div>
+        </div>
+      </header>
+    );
+}
+
+export default Header;
